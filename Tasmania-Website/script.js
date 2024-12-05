709bd903683ad9ed217c6b14dc67a9ee1b3338c5
DeveloperHeat <157921501+DeveloperHeat@users.noreply.github.com>
2024-12-05 18:14:00 +1300

    Add files via upload

---
+++ Tasmania-Website/script.js
@@ -0,0 +1,69 @@
+$(function() {
+    $('.map').maphilight();
+});
+
+function north() {
+
+document.getElementById("big").innerHTML = "North West"
+document.getElementById("default").innerHTML = "Full of lush greenery, you will find one of Tasmania’s most beautiful scenery. Tasmania is also home to Mt Roland, Table Cape and Nut. This section of Tasmania is also home to the Tarkine. The Tarkine is an area which contains the Savage River National Park. Tasmania’s North West is also home to the Cradle Mountain which is one of Tasmania’s most visited tourist destinations. Cradle Mountain offers a variety of activities to do like forest adventures and walking. It also offers some spectacular and breathtaking scenery. No matter what you choose to do, the North West of Tasmania offers heaps of fun activities to do and will keep you occupied!"
+}
+
+function launce() {
+  document.getElementById("big").innerHTML = "Launceston And North"
+  document.getElementById("default").innerHTML = "Tasmania’s North is home to lots of rich farmland, and heritage estates. The largest city in the region and second biggest city in the state is Launceston. In Launceston, you can find a vibrant hub of food, wine and culture. This city is found on the banks of the Tamar river and is also the entrance to the Tamar river. You can also find some natural beauty in the Cataract Gorge which is only a few minutes away from the city centre. For those who are interested in wildlife, there is the Narawntapu National Park and the Mt William National Park. One thing is for certain and that is you will not be bored!"
+}
+
+function east() {
+  document.getElementById("big").innerHTML = "East Coast";
+  document.getElementById("default").innerHTML = "The east coast of Tasmania offers lots of beautiful and breathtaking scenery. This place is home to one of the Lonely Planet’s top 10 regions in the world. You should definitely check out the beaches of the Bay of Fires. This area also hosts historic townships, fresh seafood and friendly locals. All of these combine to create one of the best scenic coastal drives in the country. There are five national parks spread across the coast which also includes the stunning Freycinet National Park. There is also something in it for people who are into diving. There are sheer rock walls, caves and more. This place is definitely a beautiful and scenic area and you should come down here to enjoy the breathtaking scenery!"
+}
+
+function hobart() {
+  document.getElementById("big").innerHTML = "Hobart and South";
+  document.getElementById("default").innerHTML = "Hobart is the capital city of Tasmania and is also home to incredible activities and attractions. Hobart is also home to Mount Wellington. One of the most visited attractions in Hobart is the Mona which is a famous museum. The Mona is Australia’s biggest private museum and has custom built high-tech technology. The South of Hobart, which is also the south of Tasmania, contains the Tasmanian National Park which has an incredible coastline and historic convict sites. If you are travelling north to Launceston, the Midway Highway offers many chances to learn about the history of Tasmania’s heritage with country cottages and homesteads. This is a great place to visit with its wide range of world-class attractions, you will certainly be entertained!"
+}
+
+function west() {
+  document.getElementById("big").innerHTML = "West Coast";
+  document.getElementById("default").innerHTML = "The west coast of Tasmania offers some stunning national parks and historic mining towns. This area of Tasmania is known for its historical sites which includes the site of the historic conflict between forestry workers and environmentalists to save the flooding at Lake Pedder. In the west coast, you can also find Sarah Island which was one of the harshest penal colony settlements in Australia. There are so many ways to enjoy your experience in the west coast of Tasmania whether it is from a forest or even in your car. With everything that the west coast has to offer, you are sure to have a great time!"
+}
+
+function normal() {
+  document.getElementById("default").innerHTML = "Explore these sections of Tasmania by clicking on one of the sections!" + '<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>';
+}
+
+function dam() {
+  location.replace("dam.html")
+}
+
+function back() {
+  location.replace("index.html");
+}
+
+function wine() {
+  location.replace("wine.html")
+}
+
+function kunayi() {
+  location.replace("kunayi.html");
+}
+
+function fires() {
+  location.replace("fires.html");
+}
+
+function tasman() {
+  location.replace("tasman.html")
+}
+
+function cradle() {
+  location.replace("cradle.html")
+}
+
+function man() {
+  location.replace("man.html")
+}
+
+function cat() {
+  location.replace("cat.html");
+}
